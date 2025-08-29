a2e7c48e6c80f590211929936cba00d59f5d9f03
SankalpMathur05 <sankalpmathur4@gmail.com>
2025-08-29 08:22:50 +0530

    changes

--- frontend/Task-Manager/src/utils/apiPaths.js
+++ frontend/Task-Manager/src/utils/apiPaths.js
@@ -1,4 +1,4 @@
-export const BASE_URL = import.meta.env.BACKEND_URL || "http://localhost:5000";
+export const BASE_URL = import.meta.env.VITE_BACKEND_URL || "http://localhost:5000";
 
 // utils/apiPaths.js
 export const API_PATHS = {
